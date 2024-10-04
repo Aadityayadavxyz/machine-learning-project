4b2b29e0b5dbec1a143455c6616436f768bdf4b1
Aaditya <aadityayadav698@gmail.com>
2024-10-04 14:11:01 +0530

    Done hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,8 @@
 import sys
 import os
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+
 # #def read_sql_data():
 #     logging.info('Reading sql database started')
 #     mydb = pymysql.connect(
@@ -31,11 +33,15 @@ def save_object(file_path,obj):
         
     except Exception as e:
         raise CustomException(e,sys)
-def evaluate_model(x_train, y_train,x_test,y_test,models):
+def evaluate_model(x_train, y_train,x_test,y_test,models,params):
     try:
         report = {}
         for i in range(len(models)):
             model = list(model.values())[i]
+            para = params[list(model.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
